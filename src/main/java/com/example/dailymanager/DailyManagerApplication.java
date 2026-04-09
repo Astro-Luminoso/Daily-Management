736d40250daa5ac04b44a7d1f55f9448250f6583
hanbyeol.yang <beuri97@gmail.com>
2026-04-09 12:00:09 +0900

    chore: first commit

---
+++ src/main/java/com/example/dailymanager/DailyManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.dailymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailyManagerApplication.class, args);
+    }
+
+}
