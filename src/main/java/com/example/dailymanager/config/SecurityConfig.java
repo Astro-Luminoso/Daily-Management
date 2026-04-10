25c8b40db67816bcef83f432dfa9578e11395785
hanbyeol.yang <beuri97@gmail.com>
2026-04-10 14:40:38 +0900

    docs: update comment on CSRF configuration in SecurityFilterChain

--- src/main/java/com/example/dailymanager/config/SecurityConfig.java
+++ src/main/java/com/example/dailymanager/config/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
-                .csrf(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)  /* Maybe remove this line as this api did not decide to use JWT yet */
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/events/**").permitAll()
                         .anyRequest().authenticated()
