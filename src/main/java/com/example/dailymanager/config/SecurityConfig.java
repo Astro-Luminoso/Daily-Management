622870bc0693619f4b5a63750de98e6a6641dcaa
hanbyeol.yang <beuri97@gmail.com>
2026-04-15 20:29:43 +0900

    fix: add allowed request endpoint
    - /comments were missing

--- src/main/java/com/example/dailymanager/config/SecurityConfig.java
+++ src/main/java/com/example/dailymanager/config/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
         return http
                 .csrf(AbstractHttpConfigurer::disable)  /* Maybe remove this line as this api did not decide to use JWT yet */
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/error", "/events/**").permitAll()
+                        .requestMatchers("/error", "/events/**", "/comments").permitAll()
                         .anyRequest().denyAll()
                 )
                 .build();
